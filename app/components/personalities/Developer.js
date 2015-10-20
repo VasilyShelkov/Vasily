30d4cab591f633481a0203c3486f625609b3c192
Vasily Shelkov <vasilydshelkov@gmail.com>
2015-10-20 19:38:43 +0100

    Revert "Revert "Added the initial page, set up server and package.json""
    This reverts commit 2023671831adfcd18023510742c771b403311a59.

---
+++ app/components/personalities/Developer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Developer extends Component {
+	render() {
+		return (
+			<div className="row">
+				<div className="container">
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+					<h1 className="center-align">I AM A DEVELOPER</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Developer;
